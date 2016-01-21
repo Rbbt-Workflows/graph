015254c2e0e5e11d117249b397ec82d30593eccc
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-01-21 14:23:23 +0100

    nil

--- www/views/public/js/cytoscape.js
+++ www/views/public/js/cytoscape.js
@@ -432,50 +432,9 @@ $.widget("rbbt.cytoscape_tool", {
         }})
     },
 
-    //show_info_old: function(info){
-
-    //    var table = $('<table>');
-    //    var hrow = $('<tr>');
-    //    table.append($('<thead>').append(hrow));
-
-
-    //    var rows = [];
-    //    var keys = [];
-    //    for (key in info){
-    //        keys.push(key);
-    //        hrow.append($('<th>').html(key));
-
-    //        var value = info[key];
-    //        var parts = value.split(";;");
-    //        for (i in parts){
-    //            var row = rows[i];
-    //            if (undefined === row){ row = (rows[i] = {}) }
-
-    //            row[key] = parts[i];
-    //        }
-    //    }
-
-    //    var body = $('<body>')
-    //    for (i in rows){
-    //        var row = rows[i];
-    //        var brow = $('<tr>');
-
-    //        table.append(brow)
-
-    //        for (j in keys){
-    //            var key = keys[j];
-
-    //            brow.append($('<td>').html(row[key]));
-    //        }
-    //    }
-    //    $('#modal').modal('show', table)
-
-    //},
-
     show_info: function(name, database, pair){
         var url = ['/knowledge_base/info', name, database, pair].join("/");
         rbbt.modal.controller.show_url(url)
-        //$('#modal').modal('show_url', url)
     },
 
     aesthetic: function(elem, aesthetic, map, feature){
